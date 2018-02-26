363ddd61d2b30a672f9665fa2266d46643a70498
Jimmy Song <jimmysong2018@u.northwestern.edu>
2018-02-26 11:47:20 -0600

    Added some classes

---
+++ ingredient.py
@@ -0,0 +1,21 @@
+class Ingredient:
+    def __init__(self, name = None):
+        self.name = name
+
+class Spice(Ingredient):
+    def _init_(self, name = None):
+        self.name = name
+
+class Vegetable(Ingredient):
+    def _init_(self, name = None):
+        self.name = name
+
+class Meat(Ingredient):
+    def __init__(self, name = None):
+        self.name = name
+
+class Liquid(Ingredient):
+    def __init__(self, name = None):
+        self.name = name
+
+
---
+++ recipe.py
@@ -0,0 +1,8 @@
+class Recipe:
+    def __init__(self, name = None, ingredients = [], instructions = []):
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+
+    
+
