1567546b52bc7c9bbe984fa1608d199aaa1c561e
Jimmy Song <jimmysong2018@u.northwestern.edu>
2018-02-28 20:25:55 -0600

    ingredient class redefined. everyone

--- ingredient.py
+++ ingredient.py
@@ -1,5 +1,11 @@
 class Ingredient:
-    def __init__(self, name = None, quantity = None, measurement = None, description = None, preparation = None, nutrition = None):
-        self.name = name
+    def __init__(self, name = None, quantity = None, measurement = None, descriptors = [], preparation = None, nutrition = None):
+        self.name = name #string
+        self.quantity = quantity #string
+        self.measure = measurement #string
+        self.descriptors = descriptors #list of strings
+        self.preparation = preparation #string
+        self.nutrition = nutrition #string
+
 
 
