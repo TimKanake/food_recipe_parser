0eb6e87dcfe7b68693fad4c1d88d069409fcd152
danielrthomas <danielrthomas1@gmail.com>
2018-02-28 20:02:30 -0600

    me and andre got direcitons parsed

--- scraper.py
+++ scraper.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-def scrapeIngredients(url='https://www.allrecipes.com/recipe/25333/vegan-black-bean-soup/'):
+def scrapeRecipe(url='https://www.allrecipes.com/recipe/25333/vegan-black-bean-soup/'):
     document = urllib2.urlopen(url).read()
     soup = BeautifulSoup(document, "html.parser")
     links = soup.findAll('span')
     raw_ingredients = []
+    raw_directions = []
     for link in links:
         if link.get('itemprop') != None:
             if "ingredients" in link['itemprop']:
                 raw_ingredients.append(link.string)
-    return raw_ingredients
+        if link.get('class') != None:
+            if "recipe-directions__list--item" in link['class']:
+                raw_directions.append(link.string)
+    return raw_ingredients, raw_directions
