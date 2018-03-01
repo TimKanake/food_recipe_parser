092f1e9fcee1c58d53485137f4c988447ac0be2a
danielrthomas <danielrthomas1@gmail.com>
2018-02-28 21:08:21 -0600

    dan and andre parse raw ingredients into the class structure

--- recipe_transformer.py
+++ recipe_transformer.py
@@ -1,8 +1,50 @@
-from ingredient.py import Ingredient
-from recipe.py import recipe
-from scraper.py import scrape_ingredients
+from ingredient import Ingredient
+from recipe import Recipe
+from scraper import scrapeRecipe
+
+measurement_strings = ["cup","pinch","can","tablespoon","teaspoon","clove","rack","pound","bottle","pinch"]
 
 def main():
-    scrape_ingredients()
+    ingredients = []
+    
+    r_ingredients, r_steps = scrapeRecipe()
+    for ri in r_ingredients:
+        descriptors = []
+        split_words = ri.split(' ')
+        if split_words[0][0].isnumeric():
+            quantity = split_words[0]
+            old = 1
+        else:
+            #this handles when there is no number at the start of an ungredient
+            quantity = 1
+            old = 0
+            
+        counter = old
+        halt = False
+        measurement = None
+        while counter < len(split_words) and not halt:
+            for s in measurement_strings:
+                if s in split_words[counter].lower():
+                    measurement = ' '.join(split_words[1:counter+1])
+                    halt = True
+            counter += 1
+        if measurement != None and measurement[-1] == 's':
+            measurement = measurement[:-1]
+            
+        if halt == False:
+            counter = old
+            
+        name = ' '.join(split_words[counter:])
+        preparation = None
+
+        if ',' in name:
+            preparation = name.split(',')[1]
+            name = name.split(',')[0]
+        
+        ingredients.append(Ingredient(name, quantity, measurement, [], preparation))
+                                      
+        print quantity, '|', measurement, '|', name, '|', preparation
+
+main()
 
 
