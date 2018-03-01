621b864131f66185fce65b3737752eb931761b96
danielrthomas <danielrthomas1@gmail.com>
2018-02-28 19:56:22 -0600

    me and andre got the ingredient scraper working

--- scraper.py
+++ scraper.py
@@ -1,22 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib2
-def scrapeIngredients(url):
-    if not url:
-        url = 'https://www.allrecipes.com/recipe/25333/vegan-black-bean-soup/'
+
+def scrapeIngredients(url='https://www.allrecipes.com/recipe/25333/vegan-black-bean-soup/'):
     document = urllib2.urlopen(url).read()
     soup = BeautifulSoup(document, "html.parser")
     links = soup.findAll('span')
-    print links
-    ingredients = []
+    raw_ingredients = []
     for link in links:
-        print link    
-        if "recipe-ingred_txt added" in link:
-            ingredients.append(link.string)
-    return ingredients
-
-def main():
-
-    print scrapeIngredients(False)
-    
-
-main()
+        if link.get('itemprop') != None:
+            if "ingredients" in link['itemprop']:
+                raw_ingredients.append(link.string)
+    return raw_ingredients
